6eb8b2ea1e0491f79bb940ab5b1324b1947c4c41
plummm <etenalcxz@gmail.com>
2022-10-12 11:00:30 -0700

    fix bugs: kernel debug info has more prefix than I thought

--- ftraceparser/trace.py
+++ ftraceparser/trace.py
@@ -43,7 +43,7 @@ class Trace:
         context_switcher = '------------------------------------------'
         context_switch_regx = r'(\d+)\)( )+([\<\>\(\)a-zA-Z0-9\-\_\.]+)-(\d+)( )+=>( )+([\<\>\(\)a-zA-Z0-9\-\_\.]+)-(\d+)'
         content_regx = r'(\d+)\)((.+)(\|( )+)(([A-Za-z0-9_.]+\(\))(;| {)|}))'
-        kernel_log_regx = r'\[(( )+)?(\d+\.\d+)\]\[(( )+)?T(\d+)\] (.+)'
+        kernel_log_regx = r'\[(( )+)?(\d+\.\d+)\]\[(( )+)?(T|C|P)(\d+)\] (.+)'
 
         process = {}
         ftrace_fd = open(ftrace_file, 'r')
@@ -52,7 +52,7 @@ class Trace:
         for line in texts:
             line = line.strip()
             timestamp = regx_get(kernel_log_regx, line, 2)
-            text = regx_get(kernel_log_regx, line, 6)
+            text = regx_get(kernel_log_regx, line, 7)
             if text == context_switcher:
                 continue
             if regx_match(content_regx, text):
@@ -117,6 +117,8 @@ class Trace:
 
         for i in range(start, total_line):
             if regx_match(r'CPU (\d+) is empty', self.trace_text[i]):
+                continue
+            if regx_match(r'version = \d+', self.trace_text[i]):
                 continue
             start = i
             break
